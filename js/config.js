3c25003adfa77cc89747bb3cd5998ed964937067
ezhujam <james.zhu@ericsson.com>
2021-05-28 10:23:54 +0800

    init

---
+++ js/config.js
@@ -0,0 +1,7 @@
+(function(w){
+	w.globalConfig = {
+		 host : "175.27.135.69",
+		 port : 8080
+	}
+	return w;
+})(window || {})
---
+++ js/public.js
@@ -0,0 +1,4 @@
+// axios
+// const instance = axios.create({
+// 		  baseURL: 'http://127.0.0.1:80'
+// 		});
